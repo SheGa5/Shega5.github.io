5c40971ac627e1ad97db5b71b4da83fd955b081c
Dragan Segedin <dragan.segedin@gmail.com>
2026-08-14 14:59:13 +0200

    Show Pumpkin risotto as Dynove rizoto (Pumpkin risotto) on two lines

--- lang.js
+++ lang.js
@@ -65,7 +65,7 @@
       "dish2.salmon": "Losos grilovaný, houby, pečené brambory",
       "dish2.noodlesoup_r": "Thai noodles beef soup",
       "dish2.chickensupreme": "Chicken supreme s brokolicí",
-      "dish2.pumpkinrisotto": "Pumpkin risotto",
+      "dish2.pumpkinrisotto": "Dýňové rizoto\n(Pumpkin risotto)",
       "dish2.herbchicken": "Herb fried chicken",
       "dish2.grilledveg": "Grilled vegetable mushroom",
       "dish.caesar": "Caesar salát (kuře, krevety)",
